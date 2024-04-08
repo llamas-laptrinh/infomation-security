1498960c8916020548c188f9565704baf3be8543
lammas-laptrinh <danghoangson51@gmail.com>
2024-04-08 22:59:58 +0700

    updated UI

--- modern-enctytion/Feistel Cipher/index.js
+++ modern-enctytion/Feistel Cipher/index.js
@@ -1,8 +1,17 @@
-// Hàm tạo khóa ngẫu nhiên
+function print(id, value) {
+  document.getElementById(id).innerHTML = value;
+}
+
+document.getElementById("encode-btn").addEventListener("click", () => {
+  const PT = document.getElementById("plain-text").value;
+  print("pt", PT);
+  encodeFeistelCipher(PT);
+});
+
 function rand_key(p) {
   let key = "";
   for (let i = 0; i < p; i++) {
-    const temp = Math.floor(Math.random() * 2); // Tạo bit ngẫu nhiên 0 hoặc 1
+    const temp = Math.floor(Math.random() * 2);
     key += temp.toString();
   }
   return key;
@@ -11,94 +20,121 @@ function rand_key(p) {
 // Hàm thực hiện XOR bit
 function exor(a, b) {
   let temp = "";
+  let pre = "";
   for (let i = 0; i < a.length; i++) {
     const xorResult = a[i] ^ b[i];
+    pre += `${a[i]} | ${b[i]} => ${xorResult}\n`;
     temp += xorResult.toString();
   }
-  return temp;
+  return { temp, pre };
 }
 
 // Hàm chuyển đổi nhị phân sang thập phân
 function binaryToDecimal(binary) {
-  console.log("binaryToDecimal", binary);
   return parseInt(binary, 2);
 }
-
-// Mã Feistel Cipher
-const PT = "Hello";
-console.log("Plain Text is:", PT);
-
-// Chuyển văn bản thuần sang ASCII và nhị phân
-const PT_Ascii = Array.from(PT).map((char) => char.charCodeAt(0));
-const PT_Bin = PT_Ascii.map((ascii) => ascii.toString(2).padStart(8, "0")).join(
-  ""
-);
 
 function generateCipherText(bin_data) {
   let str_data = "";
 
-  for (let i = 0; i < bin_data.length; i += 7) {
-    const temp_data = bin_data.slice(i, i + 7);
+  for (let i = 0; i < bin_data.length; i += 8) {
+    const temp_data = bin_data.slice(i, i + 8);
     const decimal_data = binaryToDecimal(temp_data);
     str_data += String.fromCharCode(decimal_data);
   }
-
-  console.log("Cipher Text:", str_data);
+  print("result", str_data);
   return str_data;
 }
+function decimalToBinary(N) {
+  return (N >>> 0).toString(2);
+}
 
-function prepareData() {
+function getText(PT) {
+  const PT_Ascii = Array.from(PT).map((char) => char.charCodeAt(0));
+  const PT_Bin = PT_Ascii.map((ascii) => ascii.toString(2).padStart(8, "0"));
+  print("ascii", PT_Ascii);
+  print("binary", PT_Bin);
+  return { PT_Bin: PT_Bin.join(""), PT_Ascii };
+}
+
+function prepareData(PT) {
+  const { PT_Bin } = getText(PT);
   const n = Math.floor(PT_Bin.length / 2);
   const L1 = PT_Bin.slice(0, n);
   const R1 = PT_Bin.slice(n);
+  print("l1", L1);
+  print("r1", R1);
   const m = R1.length;
 
   // Tạo K1 và K2
   const K1 = rand_key(m);
   const K2 = rand_key(m);
+  print("k1", K1);
+  print("k2", K2);
 
   // Vòng lặp Feistel đầu tiên
-  const f1 = exor(R1, K1);
-  const R2 = exor(f1, L1);
+  const { temp: f1, pre: pre1 } = exor(R1, K1);
+
+  print("xor1", pre1 + "\n" + f1);
+
+  const { temp: R2, pre: pre2 } = exor(f1, L1);
+  print("xor2", pre2 + "\n" + R2);
+
   const L2 = R1;
 
   // Vòng lặp Feistel thứ hai
-  const f2 = exor(R2, K2);
-  const R3 = exor(f2, L2);
+  const { temp: f2, pre: pre3 } = exor(R2, K2);
+  print("xor3", pre3 + "\n" + f2);
+
+  const { temp: R3, pre: pre4 } = exor(f2, L2);
+  print("xor4", pre4 + "\n" + R3);
+
   const L3 = R2;
   return { L3, R3, K2, K1 };
 }
 
-function encodeFeistelCipher() {
-  const { L3, R3 } = prepareData();
-  // Cipher text
+function encodeFeistelCipher(PT) {
+  const { L3, R3, K1, K2 } = prepareData(PT);
   const bin_data = L3 + R3;
-  document.writeln("Encoded Text", bin_data);
-  return generateCipherText(bin_data);
+  const encoded_str = generateCipherText(bin_data);
+
+  decodeFeistelCipher({ encoded_str, K1, K2 });
+  return;
 }
 
-function decodeFeistelCipher() {
-  const { L3, R3, K2, K1 } = prepareData();
+function decodeFeistelCipher({ encoded_str, K2, K1 }) {
+  let bin_str = "";
+  for (let i = 0; i < encoded_str.length; i++) {
+    const char = encoded_str[i];
+    const decimal = char.charCodeAt(0);
+    const binary = decimalToBinary(decimal);
+    bin_str += binary;
+  }
+
+  const n = Math.floor(bin_str.length / 2);
+  const L3 = bin_str.slice(0, n);
+  const R3 = bin_str.slice(n);
+
   const L4 = L3;
   const R4 = R3;
 
-  const f3 = exor(L4, K2);
-  const L5 = exor(R4, f3);
+  const { temp: f3 } = exor(L4, K2);
+  const { temp: L5 } = exor(R4, f3);
   const R5 = L4;
 
-  const f4 = exor(L5, K1);
-  const L6 = exor(R5, f4);
+  const { temp: f4 } = exor(L5, K1);
+  const { temp: L6 } = exor(R5, f4);
   const R6 = L5;
   const PT1 = L6 + R6;
-
-  const PT1_int = binaryToDecimal(PT1);
-  const RPT = String.fromCharCode(
-    ...Array.from(PT1_int.toString(16), (char) => parseInt(char, 16))
-  );
-  console.log("Retrieved Plain Text is: ", RPT);
-  document.writeln("Decoded Text", RPT);
-  return RPT;
+  console.log(PT1);
+  let str_data = "";
+  for (let i = 0; i < PT1.length; i += 8) {
+    const temp_data = PT1.slice(i, i + 8);
+    console.log(temp_data);
+    str_data += String.fromCharCode(binaryToDecimal(temp_data));
+  }
+  console.log(str_data);
+  return str_data;
 }
 
-encodeFeistelCipher();
-decodeFeistelCipher();
+// https://www.geeksforgeeks.org/feistel-cipher/?ref=header_search
